5648a1346bb4318281d7f43ea59fe5d5c9293f68
Sodj <>
2018-09-13 17:53:04 +0100

    react in electron

---
+++ main.js
@@ -0,0 +1,43 @@
+const {app, BrowserWindow} = require('electron');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+app.on('ready', function() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    
+    // and load the index.html of the app.
+    // mainWindow.loadFile('build/index.html'); // PROD
+    mainWindow.loadURL('http://localhost:3000'); // DEV
+    
+    
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+    
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    })
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+})
