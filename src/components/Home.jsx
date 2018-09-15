c95db1506d2634e6ea8053307478e0574f4fce7a
Sodj <lmd2se@hotmail.fr>
2018-09-15 03:37:15 +0100

    comment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+//const {remote} = window.require('electron');
 
 export default class Home extends Component {
     
