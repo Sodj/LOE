fa1d2b24c58e351eaa105fe3415fda99f9ce2fbb
Sodj <lmd2se@hotmail.fr>
2018-09-22 12:49:33 +0100

    Edit/Delete context menus

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { getLists } from '../lib/storage';
 import NewList from './newList';
-//const {remote} = window.require('electron');
+const useContextMenu = !!window.require;
+var remote, Menu, MenuItem;
+if(useContextMenu){
+    remote = window.require('electron').remote;
+    Menu = remote.Menu;
+    MenuItem = remote.MenuItem;
+}
 
 export default class Home extends Component {
     
@@ -12,16 +18,43 @@ export default class Home extends Component {
             lists: [],
             adding: false
         };
+
+        if(useContextMenu){
+            this.menu = new Menu();
+            this.rightClickPosition = null;
+        }
     }
 
+    componentWillMount() {
+        if(useContextMenu){
+            this.menu.append(new MenuItem({label: 'Edit', click: this.editList}));
+            this.menu.append(new MenuItem({label: 'Delete', click: this.deleteList}));
+        }
+    }
+    
     componentDidMount() {
         var lists = getLists();
         this.setState({lists: lists});
     }
     
+    rightClick = (name, e) => {
+        e.preventDefault();
+        this.setState({rightClicked: name});
+        this.rightClickPosition = {x: e.x, y: e.y};
+        this.menu.popup(remote.getCurrentWindow());
+    }
+    
+    editList = () => {
+        alert("edit: "+this.state.rightClicked)
+    }
+
+    deleteList = () => {
+        alert("delete: "+this.state.rightClicked)
+    }
+    
     ListItem = (props) => {
         return (
-            <Link to={"/list/"+props.name}>
+            <Link to={"/list/"+props.name} onContextMenu={useContextMenu? this.rightClick.bind(null, props.name) : null}>
                 <div className="list" style={{backgroundImage: "url('list_covers/"+props.cover+"')"}}>
                     <div className="name">{props.name}</div>
                 </div>
